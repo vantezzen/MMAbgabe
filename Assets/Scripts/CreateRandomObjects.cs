228688c53c00ce5f7d90a82c6b754c0e68b59bcf
Bennett <hello@vantezzen.io>
2021-05-19 21:02:14 +0200

    Update

--- Assets/Scripts/CreateRandomObjects.cs
+++ Assets/Scripts/CreateRandomObjects.cs
@@ -5,17 +5,24 @@ using UnityEngine;
 public class CreateRandomObjects : MonoBehaviour
 {
     public Transform coin;
+    public GameObject boden;
+
+    public float range = 4;
 
     // Start is called before the first frame update
     void Start()
     {
         // Erzuge 10 Coins mit Rotation
         for(int i = 0; i < 10; i++) {
-            Transform coinObj = Instantiate(coin, new Vector3(
-                Random.Range(-4, 4),
-                3,
-                Random.Range(-4, 4)
-            ), Quaternion.identity);
+            Transform coinObj = Instantiate(coin, boden.transform.position, Quaternion.identity);
+            
+            coinObj.transform.SetParent(boden.transform);
+            coinObj.localPosition = new Vector3(
+                coinObj.localPosition.x + Random.Range(-1 * range, range),
+                coinObj.localPosition.y + 2,
+                coinObj.localPosition.z + Random.Range(-1 * range, range)
+            );
+
             coinObj.transform.rotation = Quaternion.Euler(Random.Range(0, 365), Random.Range(0, 365), Random.Range(0, 365));
         }
     }
--- Assets/Scripts/VBMovingController.cs
+++ Assets/Scripts/VBMovingController.cs
@@ -9,23 +9,33 @@ public class VBMovingController : MonoBehaviour
     public Transform ButtonObject;
     public float bewegung;
 
+    private bool isPressed = false;
+
     /// <summary>
     /// Called when the scene is loaded
     /// </summary>
     void Start() {
         gameObject.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonPressed(OnButtonPressed);
+        gameObject.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonReleased(OnButtonReleased);
     }
-    void resetColor() {
-        ButtonObject.GetComponent<Renderer>().material.color = Color.black;
+    
+    void FixedUpdate() {
+        if (isPressed) {
+            // Bewegen
+            player.transform.position += player.transform.forward * (bewegung / 100);
+        }
     }
 
     /// <summary>
     /// Called when the virtual button has just been pressed:
     /// </summary>
     public void OnButtonPressed(VirtualButtonBehaviour vb) {
-        // Bewegen
-        player.transform.position += player.transform.forward * bewegung;
+        isPressed = true;
         ButtonObject.GetComponent<Renderer>().material.color = Color.blue;
-        Invoke("resetColor", 1);
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb) {
+        isPressed = false;
+        ButtonObject.GetComponent<Renderer>().material.color = Color.black;
     }
 }
